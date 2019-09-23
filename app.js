2fb4aa2a15e23e5bcaa86a3d20def6d0a6c72c01
pramithap <n9611941@qut.edu.au>
2019-09-23 18:30:35 +1000

    Changes to layout

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var createError = require('http-errors');
 var express = require('express');
+var app = express();
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var createError = require('http-errors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//paths
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -37,5 +39,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000,function() {});
+
 
 module.exports = app;
